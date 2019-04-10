3f239bda9e0aef1f4b3c722ba1d33c3a1c2e6c1e
Vanildo Prates <vprates@gmail.com>
2019-04-10 16:05:09 -0300

    Add basic models

---
+++ db/migrate/20190410190122_create_members.rb
@@ -0,0 +1,12 @@
+class CreateMembers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :members do |t|
+      t.string :name
+      t.references :campaign, foreign_key: true
+      t.boolean :open
+      t.string :token
+
+      t.timestamps
+    end
+  end
+end
